f46b605e30e1dd95d445861236e5c0606f54e89e
George Taylor <hey@george.dev>
2024-12-31 13:00:18 +0000

    chore: refactor install to functions

--- setup.sh
+++ setup.sh
@@ -1,102 +1,103 @@
 #!/bin/zsh
 
+install_oh_my_zsh() {
+    # check if zsh is installed
+    if ! command -v zsh &>/dev/null; then
+        echo "zsh is not installed. Please install zsh before running this script"
+        exit 1
+    fi
+    # check if oh-my-zsh is installed
+    if [ -d "$HOME/.oh-my-zsh" ]; then
+        echo "oh-my-zsh is already installed"
+        return
+    fi
+    # Install oh-my-zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+install_homebrew() {
+    # check if homebrew is installed
+    if ! command -v brew &>/dev/null; then
+        echo "Homebrew is not installed. Installing Homebrew"
+    else
+        echo "Homebrew is already installed"
+        return
+    fi
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+install_homebrew_packages() {
+    list_str=$1
+    list=$(echo $list_str | tr "," "\n")
+    for package in $list; do
+        brew install $package
+    done
+}
+
+install_homebrew_casks() {
+    list_str=$1
+    list=$(echo $list_str | tr "," "\n")
+    for cask in $list; do
+        brew install --cask $cask
+    done
+}
+
 # Parameters
 
 # $1: type of setup - personal or work
 
 type=$(echo "$1" | xargs)
 if [ "$type" != "personal" ] && [ "$type" != "work" ]; then
-  echo "Invalid setup type. Please provide either 'personal' or 'work' as the first argument"
-  exit 1
+    echo "Invalid setup type. Please provide either 'personal' or 'work' as the first argument"
+    exit 1
 fi
 
 echo "Setting up $type environment"
 
 vault_name=$(echo "$2" | xargs)
 if [ -z "$vault_name" ]; then
-  echo "Please provide the 1Password vault name as the second argument"
-  exit 1
+    echo "Please provide the 1Password vault name as the second argument"
+    exit 1
 fi
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Check for --skip-install flag
+skip_install=false
+for arg in "$@"; do
+    # if not skip_install flag run install function
+    if [ "$arg" = "--skip-install" ]; then
+        echo "Skipping installation of Oh-My-Zsh and Homebrew + packages/casks - only updating dotfiles"
+        skip_install=true
+    else
+        skip_install=false
+    fi
+done
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ "$skip_install" = false ]; then
+    echo "Installing Oh-My-Zsh"
+    install_oh_my_zsh
+    echo "Installing Homebrew"
+    install_homebrew
+    echo "Installing Homebrew packages"
+    install_homebrew_packages gh, awscli, terraform, python, uv, ruff, git, fzf, jq, kubectl, helm, curl, grep, openssh, eza
+    echo "Installing Homebrew casks"
+    install_homebrew_casks ghostty, antidote, docker, font-monaspace
+fi
 
-# Install Homebrew packages
+# alias eza as ls
+echo "alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'" >>~/.zshrc
 
-## Install ghostty terminal
-brew install --cask ghostty
-
-## Install antidote (for zsh plugins)
-brew install antidote
-
-## Install gh (github cli)
-brew install gh
-
-## Install vscode insiders
-brew install --cask visual-studio-code@insiders
-
-## Install one-password
-brew install --cask 1password
-
-## Install 1password cli
-brew install --cask 1password-cli
-
-## Install docker
-brew install --cask docker
-
-## Install aws cli  
-brew install awscli
-
-## Install terraform
-brew install terraform
-
-## Install python
-brew install python
-## Install uv and ruff
-brew install uv ruff
-
-## Install git
-brew install git
-
-## Install fzf
-brew install fzf
-
-## Install jq
-brew install jq
-
-## Install kubectl
-brew install kubectl
-
-## Install helm
-brew install helm
-
-## Install curl
-brew install curl
-
-## Install monaspace font
-brew install --cask font-monaspace
-
-## Install eza (enhanced ls)
-brew install eza
-echo "alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'" >> ~/.zshrc
-
-## Update some tools
-brew install grep
-brew install openssh
-
-# Remove outdated versions from the cellar.
+# Remove outdated versions from the cellar.\
+echo "Cleaning up Homebrew"
 brew cleanup
 
 # Configure zsh plugins
 cp .zsh/zsh_plugins.txt ~/.zsh_plugins.txt
 echo "reload zsh to apply changes"
-echo "run: source ~/.zshrc"
+echo "run: exec zsh"
 
-echo "export EDITOR="code-insiders -w" >> ~/.zshrc
-cat > ~/.zshrc <<EOF
+echo "export EDITOR=\"code-insiders -w\"" >>~/.zshrc
+cat >>~/.zshrc <<EOF
 source $HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh
 antidote load ${ZDOTDIR:-$HOME}/.zsh_plugins.txt
 autoload -Uz promptinit && promptinit && prompt pure
@@ -108,39 +109,42 @@ complete -o nospace -C $HOMEBREW_PREFIX/bin/terraform terraform
 EOF
 
 # Configure fzf
-echo "eval \"\$(fzf --zsh)\"" >> ~/.zshrc
+echo "eval \"\$(fzf --zsh)\"" >>~/.zshrc
 
 # Configure eza as ls
 # alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'
-echo "alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'" >> ~/.zshrc
+if ! grep -Fxq "alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'" ~/.zshrc; then
+    echo "alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'" >>~/.zshrc
+fi
 
 # replace zsh plugins line in zshrc file
 sed -i '' 's/^plugins=.*/plugins=(git aws docker brew emoji gh python terraform common-aliases)/g' ~/.zshrc
 
 # auto update oh-my-zsh
-echo "zstyle ':omz:update' check 1" >> ~/.zshrc
-echo "zstyle ':omz:update' mode auto" >> ~/.zshrc
+echo "zstyle ':omz:update' check 1" >>~/.zshrc
+echo "zstyle ':omz:update' mode auto" >>~/.zshrc
 
-# set zsh history size unlimited
-echo "HISTSIZE=-1" >> ~/.zshrc
-echo "SAVEHIST=-1" >> ~/.zshrc
+# set zsh history size to very large
+echo "HISTSIZE=999999999" >>~/.zshrc
+echo "SAVEHIST=999999999" >>~/.zshrc
+echo "HISTFILE=~/.zsh_history" >>~/.zshrc
 
 # set zsh autosuggest strategy
-echo "ZSH_AUTOSUGGEST_STRATEGY=(history completion)" >> ~/.zshrc
+echo "ZSH_AUTOSUGGEST_STRATEGY=(history completion)" >>~/.zshrc
 
 # set zsh history stamp format
-echo "HIST_STAMPS='dd/mm/yyyy'" >> ~/.zshrc
+echo "HIST_STAMPS='dd/mm/yyyy'" >>~/.zshrc
 
 # cp other dotfiles/configs
-cp .gitconfig ~/.gitconfig
+# cp .gitconfig ~/.gitconfig
 cp .gitignore ~/.gitignore
-cp .ghostty/config ~/.config/ghostty/config
+cp ./ghostty/config ~/.config/ghostty/config
 
 # Setup 1password ssh agent
-echo "export SSH_AUTH_SOCK=~/.1password/agent.sock" >> ~/.zshrc
+echo "export SSH_AUTH_SOCK=~/.1password/agent.sock" >>~/.zshrc
 mkdir -p ~/.config/1Password/ssh
 touch ~/.config/1Password/ssh/config
-cat > ~/.config/1Password/ssh/config <<EOF
+cat >~/.config/1Password/ssh/config <<EOF
 [[ssh-keys]]
 vault = "$vault_name"
 EOF
